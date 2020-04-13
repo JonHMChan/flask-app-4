3885e3c4c94eecd0c1e65ec2e13de82e8c44e81d
Jon Chan <me@jonhmchan.com>
2020-04-13 09:13:00 -0700

    Add Postgres connection by URL and .env

--- data/db.py
+++ data/db.py
@@ -1,3 +1,16 @@
 import psycopg2
+import os
+from urllib.parse import urlparse
 
-conn = psycopg2.connect("dbname=pokedex user=postgres password=postgres port=5432 host=localhost")
+result = urlparse(os.getenv("DATABASE_URL"))
+
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+conn = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname
+)
