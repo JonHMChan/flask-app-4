f50ce64c9ec6584849737d3a44b299e24b79c55c
kristinalustig <kristina.lustig@gmail.com>
2020-04-15 10:18:03 -0400

    Made schema idempotent, updated naming conventions as per PR comments

--- data/schema.sql
+++ data/schema.sql
@@ -1,51 +1,32 @@
-CREATE DATABASE pokedex;
+IF NOT EXISTS(SELECT * FROM pokedex)
+BEGIN
+    CREATE DATABASE pokedex;
 
-CREATE TABLE teams (
-    team_id INTEGER PRIMARY KEY,
-    team_name VARCHAR(50),
-    team_desc VARCHAR(500)
-);
+    CREATE TABLE teams (
+        id INTEGER PRIMARY KEY,
+        _name TEXT,
+        _desc TEXT
+    );
 
-CREATE TABLE pokemon (
-    poke_id INTEGER PRIMARY KEY,
-    poke_name VARCHAR(50),
-    poke_desc VARCHAR(500),
-    poke_image_url VARCHAR(255),
-    poke_type_1 VARCHAR(10),
-    poke_type_2 VARCHAR(10)
-);
+    CREATE TABLE pokemon (
+        id INTEGER PRIMARY KEY,
+        _name TEXT,
+        _desc TEXT,
+        image_url TEXT,
+        type_1 TEXT,
+        type_2 TEXT
+    );
 
-CREATE TABLE team_members (
-    team_id INTEGER REFERENCES teams(team_id),
-    poke_id INTEGER REFERENCES pokemon(poke_id),
-    member_level INTEGER
-);
+    CREATE TABLE team_members (
+        teams_id INTEGER REFERENCES teams(id),
+        pokemon_id INTEGER REFERENCES pokemon(id),
+        member_level INTEGER
+    );
 
-/*CREATE TABLE types (
-    poke_id INTEGER PRIMARY KEY REFERENCES pokemon(poke_id),
-    is_normal BOOLEAN,
-    is_fire BOOLEAN,
-    is_water BOOLEAN,
-    is_ground BOOLEAN,
-    is_grass BOOLEAN,
-    is_electric BOOLEAN,
-    is_ice BOOLEAN,
-    is_fighting BOOLEAN,
-    is_poison BOOLEAN,
-    is_flying BOOLEAN,
-    is_psychic BOOLEAN,
-    is_bug BOOLEAN,
-    is_rock BOOLEAN,
-    is_ghost BOOLEAN,
-    is_dark BOOLEAN,
-    is_dragon BOOLEAN,
-    is_steel BOOLEAN,
-    is_fairy BOOLEAN
-);*/
-
-CREATE TABLE evolutions (
-    poke_id INTEGER REFERENCES pokemon(poke_id),
-    evol_id INTEGER REFERENCES pokemon(poke_id),
-    evol_method VARCHAR(20),
-    evol_level INTEGER
-);
+    CREATE TABLE evolutions (
+        pokemon_id INTEGER REFERENCES pokemon(id),
+        evol_id INTEGER REFERENCES pokemon(id),
+        evol_method TEXT,
+        evol_level INTEGER
+    );
+END;
